f331b71b2a18fa1dcb4204014d50a791eb8aa7c1
Arthurias6 <maolin1698@gmail.com>
2025-03-08 22:28:05 +0100

    More changes

--- init.lua
+++ init.lua
@@ -1,16 +1,2 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-vim.g.have_nerd_font = true
-
--- [[ Setting options ]]
-require 'config.options'
-
--- [[ Basic Keymaps ]]
-require 'config.keymaps'
-
--- [[ Install `lazy.nvim` plugin manager ]]
-require 'config.lazy-bootstrap'
-
--- [[ Configure and install plugins ]]
-require 'config.lazy-plugins'
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
